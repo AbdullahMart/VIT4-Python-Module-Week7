920987efbeba3da7a293e0ffeaf45f42794e0e27
ERKHAN KOZHA <erkhankozha@gmail.com>
2024-05-09 18:13:11 +0200

    updated

--- preference_admin_menu.py
+++ preference_admin_menu.py
@@ -12,24 +12,30 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 class Ui_admin_pref_men_MainWindow(object):
     def setupUi(self, admin_pref_men_MainWindow):
         admin_pref_men_MainWindow.setObjectName("admin_pref_men_MainWindow")
-        admin_pref_men_MainWindow.resize(800, 600)
+        admin_pref_men_MainWindow.resize(500, 500)
+        admin_pref_men_MainWindow.setMinimumSize(QtCore.QSize(500, 500))
+        admin_pref_men_MainWindow.setMaximumSize(QtCore.QSize(500, 500))
         admin_pref_men_MainWindow.setAcceptDrops(False)
         admin_pref_men_MainWindow.setLayoutDirection(QtCore.Qt.LayoutDirection.LeftToRight)
         admin_pref_men_MainWindow.setAutoFillBackground(False)
         self.centralwidget = QtWidgets.QWidget(parent=admin_pref_men_MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.admin_preference_menu_frame = QtWidgets.QFrame(parent=self.centralwidget)
-        self.admin_preference_menu_frame.setGeometry(QtCore.QRect(-140, -100, 721, 661))
+        self.admin_preference_menu_frame.setGeometry(QtCore.QRect(-150, -100, 721, 661))
         self.admin_preference_menu_frame.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
         self.admin_preference_menu_frame.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
         self.admin_preference_menu_frame.setObjectName("admin_preference_menu_frame")
         self.werhere_image_label = QtWidgets.QLabel(parent=self.admin_preference_menu_frame)
-        self.werhere_image_label.setGeometry(QtCore.QRect(210, 110, 481, 101))
+        self.werhere_image_label.setGeometry(QtCore.QRect(170, 110, 481, 101))
         self.werhere_image_label.setText("")
-        self.werhere_image_label.setPixmap(QtGui.QPixmap("../../Downloads/IMG-20240507-WA0026.jpg"))
+        self.werhere_image_label.setPixmap(QtGui.QPixmap("werhere_image.png"))
         self.werhere_image_label.setObjectName("werhere_image_label")
         self.application_page_pushButton = QtWidgets.QPushButton(parent=self.admin_preference_menu_frame)
-        self.application_page_pushButton.setGeometry(QtCore.QRect(220, 240, 75, 24))
+        self.application_page_pushButton.setGeometry(QtCore.QRect(190, 240, 151, 61))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        self.application_page_pushButton.setFont(font)
         self.application_page_pushButton.setStyleSheet("QPushButton:hover {\n"
 "\n"
 "                  background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(0,157, 255, 255), stop:1 rgba(30, 206, 255, 255));\n"
@@ -42,7 +48,11 @@ class Ui_admin_pref_men_MainWindow(object):
 "                    }")
         self.application_page_pushButton.setObjectName("application_page_pushButton")
         self.mentor_interview_pushButton = QtWidgets.QPushButton(parent=self.admin_preference_menu_frame)
-        self.mentor_interview_pushButton.setGeometry(QtCore.QRect(460, 240, 101, 31))
+        self.mentor_interview_pushButton.setGeometry(QtCore.QRect(430, 240, 181, 61))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        self.mentor_interview_pushButton.setFont(font)
         self.mentor_interview_pushButton.setStyleSheet("QPushButton:hover {\n"
 "\n"
 "                  background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(0,157, 255, 255), stop:1 rgba(30, 206, 255, 255));\n"
@@ -55,7 +65,11 @@ class Ui_admin_pref_men_MainWindow(object):
 "                    }")
         self.mentor_interview_pushButton.setObjectName("mentor_interview_pushButton")
         self.Exit_pushButton = QtWidgets.QPushButton(parent=self.admin_preference_menu_frame)
-        self.Exit_pushButton.setGeometry(QtCore.QRect(460, 370, 101, 31))
+        self.Exit_pushButton.setGeometry(QtCore.QRect(340, 460, 111, 61))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        self.Exit_pushButton.setFont(font)
         self.Exit_pushButton.setStyleSheet("QPushButton:hover {\n"
 "\n"
 "                  background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(0,157, 255, 255), stop:1 rgba(30, 206, 255, 255));\n"
@@ -68,7 +82,11 @@ class Ui_admin_pref_men_MainWindow(object):
 "                    }")
         self.Exit_pushButton.setObjectName("Exit_pushButton")
         self.interviews_pushButton = QtWidgets.QPushButton(parent=self.admin_preference_menu_frame)
-        self.interviews_pushButton.setGeometry(QtCore.QRect(220, 360, 81, 31))
+        self.interviews_pushButton.setGeometry(QtCore.QRect(190, 360, 151, 61))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        self.interviews_pushButton.setFont(font)
         self.interviews_pushButton.setStyleSheet("QPushButton:hover {\n"
 "\n"
 "                  background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(0,157, 255, 255), stop:1 rgba(30, 206, 255, 255));\n"
@@ -81,7 +99,11 @@ class Ui_admin_pref_men_MainWindow(object):
 "                    }")
         self.interviews_pushButton.setObjectName("interviews_pushButton")
         self.back_menu_pushButton = QtWidgets.QPushButton(parent=self.admin_preference_menu_frame)
-        self.back_menu_pushButton.setGeometry(QtCore.QRect(230, 470, 75, 24))
+        self.back_menu_pushButton.setGeometry(QtCore.QRect(430, 360, 181, 61))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        self.back_menu_pushButton.setFont(font)
         self.back_menu_pushButton.setStyleSheet("QPushButton:hover {\n"
 "\n"
 "                  background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(0,157, 255, 255), stop:1 rgba(30, 206, 255, 255));\n"
@@ -95,7 +117,7 @@ class Ui_admin_pref_men_MainWindow(object):
         self.back_menu_pushButton.setObjectName("back_menu_pushButton")
         admin_pref_men_MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(parent=admin_pref_men_MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 500, 22))
         self.menubar.setObjectName("menubar")
         admin_pref_men_MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(parent=admin_pref_men_MainWindow)
